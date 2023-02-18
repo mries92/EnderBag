56a3c6cc202969de3e2ddf4e9f1e652b00a68d4e
Mike Ries <mries92@gmail.com>
2023-02-17 20:26:05 -0500

    Adding defaults for config values

--- EnderBag/src/main/java/org/mries/enderbag/config/EnderBagConfig.java
+++ EnderBag/src/main/java/org/mries/enderbag/config/EnderBagConfig.java
@@ -2,19 +2,25 @@ package org.mries.enderbag.config;
 
 import org.bukkit.configuration.file.FileConfiguration;
 
+import java.util.Arrays;
 import java.util.List;
 
 @SuppressWarnings("unchecked")
 public class EnderBagConfig {
     public EnderBagConfig(FileConfiguration config) {
-        itemName = config.getString("item-name");
-        itemDescription = config.getString("item-description");
-        showDescription = config.getBoolean("show-description");
-        appearEnchanted = config.getBoolean("appear-enchanted");
-        baseItem = config.getString("base-item");
-        uid = config.getInt("uid");
+        itemName = config.getString("item-name", "§aEnder Bag");
+        itemDescription = config.getString("item-description", "§bOpen your ender chest");
+        showDescription = config.getBoolean("show-description", true);
+        appearEnchanted = config.getBoolean("appear-enchanted", false);
+        baseItem = config.getString("base-item", "ender_eye");
+        uid = config.getInt("uid", 10);
 
+        config.addDefault("recipe.values", Arrays.asList("DED", "ECE", "DED"));
         recipeValues = config.getStringList("recipe.values");
+        config.addDefault("recipe.key", Arrays.asList(
+                Arrays.asList("D", "diamond"),
+                Arrays.asList("E", "ender_eye"),
+                Arrays.asList("C", "ender_chest")));
         recipeKeys = (List<List<String>>) config.getList("recipe.key");
     }
 
